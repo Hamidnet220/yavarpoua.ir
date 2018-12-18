a0dc3cd0c7b369e105a5f1bde18d546b37ac7626
=Hamidnet220 <kiani.hamidreza@gmail.com>
2018-12-18 16:10:54 +0330

    Add mongoose and connect ot local Database

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const mongoose=require('mongoose');
 const app=express();
 const port=3000;
 
-mongoose.connect('localhost:2710')
+mongoose.connect('mongodb://localhost/yavarpoua')
+    .then(()=>console.log('Connected to MongoDb...'))
+    .catch(err=>console.error('Could not connect to Mongodb...',err))
+
 app.get('/',(req,res)=>{
     res.send("Hello There This Website of Yavarpoua-e-jnoob.");
 });
