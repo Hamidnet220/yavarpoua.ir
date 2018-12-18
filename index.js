6abef47a111b520831b7eaea641063d459332e0b
=Hamidnet220 <kiani.hamidreza@gmail.com>
2018-12-18 19:38:56 +0330

    add function to insert employee to database

--- index.js
+++ index.js
@@ -7,7 +7,38 @@ const port=3000;
 
 mongoose.connect('mongodb://localhost/yavarpoua')
     .then(()=>console.log('Connected to MongoDb...'))
-    .catch(err=>console.error('Could not connect to Mongodb...',err))
+    .catch(err=>console.error('Could not connect to Mongodb...',err));
+
+const employeeSchema=new mongoose.Schema({
+    year:string,
+    month:string,
+    name:string,
+    lastname:string,
+    fatherNamew:string,
+    nationalCode:string,
+    groseWage:number,
+    netPay:number
+});
+
+const Employee=mongoose.model('Employee',employeeSchema);
+
+async function createEmployee(){
+    const employee=new Employee({
+        year:'1397',
+        month:'08',
+        name:'شاکر',
+        lastname:'آبرفت',
+        fatherNamew:'حسن',
+        nationalCode:'175125276',
+        groseWage:25000000,
+        netPay:21000000
+    });
+    
+    const result=await employee.save();
+    console.log(result);
+}
+
+
 
 app.get('/',(req,res)=>{
     res.send("Hello There This Website of Yavarpoua-e-jnoob.");
